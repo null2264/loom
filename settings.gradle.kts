ffc52b29cf5b30096d4fc17f616c82df0770a34d
Ahmad Ansori Palembani <palembani@gmail.com>
2025-04-29 18:53:44 +0700

    refactor: Clean up unused files

--- settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-tryIncludeBuild("null-fabric-loom")
-tryIncludeBuild("null-architectury-loom")
-
-fun tryIncludeBuild(path: String) {
-    if (file(path).exists()) includeBuild(path)
-}
-
-rootProject.name = "null-loom-parent"
